d3e31d4f079f14830d7059e73a9f6bbc3670cb29
Axel Wagner <mail@merovius.de>
2015-04-04 17:49:58 +0200

    Gracefully shutdown on SIGINT and SIGTERM

--- main.go
+++ main.go
@@ -1,17 +1,46 @@
 package main
 
-import "log"
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
 
 func main() {
 	r, err := NewNFCReader("")
 	if err != nil {
 		log.Fatal(err)
 	}
-	for {
-		uid, err := r.GetNextUID()
-		if err != nil {
-			log.Fatal(err)
+
+	sigs := make(chan os.Signal)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	uids := make(chan []byte)
+	go func() {
+		for {
+			uid, err := r.GetNextUID()
+			if err != nil {
+				log.Println(err)
+				close(sigs)
+				return
+			}
+			uids <- uid
 		}
-		log.Printf("Got UID %x", uid)
+	}()
+
+	for {
+		select {
+		case uid := <-uids:
+			log.Printf("Got UID %x", uid)
+		case sig := <-sigs:
+			if sig != nil {
+				log.Printf("Got signal %s, exiting\n", sig)
+			}
+			if err := r.Close(); err != nil {
+				log.Println("Error closing reader:", err)
+			}
+			return
+		}
 	}
 }
