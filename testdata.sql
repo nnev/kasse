d78706cbd1c4da8001fdca8f0d0eea4b7c2651be
Axel Wagner <mail@merovius.de>
2015-09-12 22:11:07 +0200

    Add description to testdata

--- testdata.sql
+++ testdata.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (user_id, name, password) VALUES (1, 'Merovius', '$2a$10$itZjdNwMSxCRVXepc61mue2DybeIPAqx/7pia4iONZluluiRdIVmq');
 INSERT INTO users (user_id, name, password) VALUES (2, 'Koebi', '$2a$10$6.LdtTXyxK9o13Qi6u97ceQNMbf.rYiO8IlQMCdIWfBEoREAnwlWO');
 
-INSERT INTO cards (card_id, user_id) VALUES (x'61616161', 1);
-INSERT INTO cards (card_id, user_id) VALUES (x'61616162', 1);
-INSERT INTO cards (card_id, user_id) VALUES (x'62616161', 2);
-INSERT INTO cards (card_id, user_id) VALUES (x'62616162', 2);
+INSERT INTO cards (card_id, user_id, description) VALUES (x'61616161', 1, '');
+INSERT INTO cards (card_id, user_id, description) VALUES (x'61616162', 1, '');
+INSERT INTO cards (card_id, user_id, description) VALUES (x'62616161', 2, '');
+INSERT INTO cards (card_id, user_id, description) VALUES (x'62616162', 2, '');
 
 INSERT INTO transactions (user_id, card_id, time, amount, kind) VALUES (1, NULL, '2015-04-06 22:59:03', 1000, 'Aufladung');
 INSERT INTO transactions (user_id, card_id, time, amount, kind) VALUES (1, x'61616161', '2015-04-06 23:05:27', -100, 'Kartenswipe');
